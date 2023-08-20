e3217c798479b6b80b575b3c4b7f1ce6ae1f95e3
Halit <ha.lit10@hotmail.com>
2023-08-20 22:12:25 +0300

    Efekt paketi eklendi ve Bomba efekti eklendi.

--- Assets/Bullet Bounce/Game/Code/Obstacle.cs
+++ Assets/Bullet Bounce/Game/Code/Obstacle.cs
@@ -27,16 +27,21 @@ namespace S_Durlanik.Game
         public LayerMask platformRbLayer;
         public float tntRadius = 2f;
         public float tntForce = 2f;
+        public GameObject tntExplosionEffect;
         
         public void TntExplode()
         {
             var enemies = Physics2D.OverlapCircleAll(transform.position, tntRadius, enemyLayer);
 
+            //var middlePoint = Vector3.zero;
             foreach (Collider2D enemy in enemies)
             {
+                
+                //middlePoint += enemy.transform.position;
                 enemy.GetComponent<Enemy>().DestroyEnemy(0,GetComponent<Rigidbody2D>());
             }
-            
+            //middlePoint /= enemies.Length;
+
             var obstacles = Physics2D.OverlapCircleAll(transform.position, tntRadius, obstacleLayer);
             var platformRigidbodies = Physics2D.OverlapCircleAll(transform.position, tntRadius, platformRbLayer);
             
@@ -56,7 +61,8 @@ namespace S_Durlanik.Game
                     obstacleRigidbody.AddForce(direction * tntForce, ForceMode2D.Impulse);
                 }
             }
-
+            GameObject effect = Instantiate(tntExplosionEffect, transform.position + new Vector3(0,0,-20f), Quaternion.identity);
+            effect.transform.localScale = new Vector3(tntRadius, tntRadius, 1f) * .5f;
             OnTntExplode?.Invoke();
             DestroyObstacle();
         }
